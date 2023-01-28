1a7d07455ede18c6a1d047c98e6d48d4db2b8761
Christophe <christophe.dame@camunda.com>
2023-01-28 17:14:26 +0100

    collapse expand errors logs

--- src/main/front/src/pages/ConnectorErrors.tsx
+++ src/main/front/src/pages/ConnectorErrors.tsx
@@ -9,6 +9,9 @@ function ConnectorErrors() {
 
   const [errors, setErrors] = useState<any[]|null>(null);
   const [connector, setConnector] = useState<string|null>(null);
+  const [showException, setShowException] = useState<boolean>(false);
+  const [showVariables, setShowVariables] = useState<boolean>(false);
+  const [showConnector, setShowConnector] = useState<boolean>(false);
   
   const loadDashboard = async () => {
     let url = window.location.href;
@@ -27,16 +30,18 @@ function ConnectorErrors() {
     errors ?
       <>
         <h2>Connector <b>{connector}</b></h2>
-      <Accordion defaultActiveKey="0">
+        <Accordion defaultActiveKey="0">
         {errors.map((error: any, index: number) =>
           <Accordion.Item key={index} eventKey={'' + index} >
             <Accordion.Header>Instance&nbsp;<b>{error.processInstance}</b>&nbsp;on&nbsp;<i>{error.date}</i></Accordion.Header>
             <Accordion.Body>
-              <Table variant="secondary" striped bordered hover>
+              <Table variant="secondary" striped bordered>
                 <tbody>
-                  <tr><td>Exception</td><td>{error.exception}</td></tr>
-                  <tr><td>Connector</td><td dangerouslySetInnerHTML={{ __html: JSON.stringify(error.connector, null, 2).replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp;&nbsp;') }}></td></tr>
-                  <tr><td>Variables</td><td dangerouslySetInnerHTML={{ __html: JSON.stringify(error.context, null, 2).replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp;&nbsp;') }}></td></tr>
+                  <tr className={showException ? '' : 'collapsed'} ><td>Exception</td><td><div>{error.exception}</div></td><td onClick={() => setShowException(!showException)}><i className={showException ? "bi bi-arrows-collapse" : "bi bi-arrows-expand"}></i></td></tr>
+                  <tr className={showConnector ? '' : 'collapsed'}><td>Connector</td><td dangerouslySetInnerHTML={{ __html: showConnector ? JSON.stringify(error.connector, null, 2).replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp;&nbsp;') : '<div>' + JSON.stringify(error.connector) + '</div>'}}></td>
+                    <td onClick={() => setShowConnector(!showConnector)}><i className={showConnector ? "bi bi-arrows-collapse" : "bi bi-arrows-expand"}></i></td></tr>
+                  <tr className={showVariables ? '' : 'collapsed'}><td>Variables</td><td dangerouslySetInnerHTML={{ __html: showVariables ? JSON.stringify(error.context, null, 2).replaceAll('\n', '<br/>').replaceAll(' ', '&nbsp;&nbsp;') : '<div>' + JSON.stringify(error.context) + '</div>'}}></td>
+                    <td onClick={() => setShowVariables(!showVariables)}><i className={showVariables ? "bi bi-arrows-collapse":"bi bi-arrows-expand"}></i></td></tr>
                 </tbody>
                </Table>
             </Accordion.Body>
